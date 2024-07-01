7077845112b3d121f2ba513ab8ee6e3026619bb9
Dana <danamerro@gmail.com>
2024-06-30 21:08:16 -0300

    se empezó a consumir la api de pokemon y actualmente esta retornando el nombre de los pokemones desde la parte de evoluciones

--- src/App.tsx
+++ src/App.tsx
@@ -13,23 +13,30 @@ import { useState, useEffect } from "react";
 const App = () => {
   //actualiza la variable 1 en 1
   const [pokemonId, setPokemonId] = useState(1);
+  const [pokemonName, setPokemonName] = useState("");
 
-  useEffect(()=>{
-    console.log(pokemonId)
-  })
+  useEffect(() => {
+    getEvolutions(pokemonId);
+    console.log('useEffect ejecutado')
+  }, [pokemonId]);
+
+  async function getEvolutions(id: number) {
+    const response = await fetch(
+      `https://pokeapi.co/api/v2/evolution-chain/${id}/`
+    );
+    const data = await response.json();
+    setPokemonName(data.chain.species.name);
+  }
 
   //logica boton atras
   function prevClick() {
     setPokemonId((prevId) => (prevId === 1 ? 1 : prevId - 1));
   }
 
-
   //logica boton adelante
   function nextClick() {
     setPokemonId((prevId) => prevId + 1);
   }
-
- 
 
   return (
     <>
@@ -39,11 +46,9 @@ const App = () => {
 
       <div className="buttons-container">
         <Button icon={<TiArrowLeftOutline />} handleClick={prevClick} />
-        {pokemonId}
+        {pokemonName}
         <Button icon={<TiArrowRightOutline />} handleClick={nextClick} />
       </div>
-
-      
     </>
   );
 };
