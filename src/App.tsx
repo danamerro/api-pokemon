03e9607f24bbd798785e15aac44ffa147f850f7a
Dana <danamerro@gmail.com>
2024-07-04 18:52:37 -0300

    se agregaron las imagenes y más logica para los pokemones que solo tienen 1 nivel o 2, estos estan centrados y tiene sus animaciones

--- src/App.tsx
+++ src/App.tsx
@@ -9,11 +9,16 @@ import { TiArrowRightOutline } from "react-icons/ti";
 //Hooks
 import { useState, useEffect } from "react";
 
+// Definir el tipo para las evoluciones de Pokémon
+type PokemonEvolution = [string, string];
+
 //crear componente
 const App = () => {
   //actualiza la variable 1 en 1
   const [pokemonId, setPokemonId] = useState(1);
-  const [pokemonEvolutions, setPokemonEvolutions] = useState([])
+  const [pokemonEvolutions, setPokemonEvolutions] = useState<
+    PokemonEvolution[]
+  >([]);
 
   useEffect(() => {
     getEvolutions(pokemonId);
@@ -26,40 +31,35 @@ const App = () => {
     );
     const data = await response.json();
 
-    let pokemonEvoArray = []
+    let pokemonEvoArray: PokemonEvolution[] = [];
 
     //arreglo con nombre e imagen
-    let pokemonLv1 = data.chain.species.name
-    let pokemonLv1Img = await getPokemonImgs(pokemonLv1)
-    pokemonEvoArray.push([pokemonLv1, pokemonLv1Img])
+    let pokemonLv1 = data.chain.species.name;
+    let pokemonLv1Img = await getPokemonImgs(pokemonLv1);
+    pokemonEvoArray.push([pokemonLv1, pokemonLv1Img]);
 
-    if(data.chain.evolves_to.length !== 0){
-      let pokemonLv2 = data.chain.evolves_to[0].species
-      .name;
-      let pokemonLv2Img = await getPokemonImgs(pokemonLv2)
-      pokemonEvoArray.push([pokemonLv2, pokemonLv2Img])
+    if (data.chain.evolves_to.length !== 0) {
+      let pokemonLv2 = data.chain.evolves_to[0].species.name;
+      let pokemonLv2Img = await getPokemonImgs(pokemonLv2);
+      pokemonEvoArray.push([pokemonLv2, pokemonLv2Img]);
       //console.log(pokemonEvoArray)
 
-      if(data.chain.evolves_to[0].evolves_to.length !==0){
-        let pokemonLv3 = data.chain.evolves_to[0].species
-      .name;
-        let pokemonLv3Img = await getPokemonImgs(pokemonLv3)
-      pokemonEvoArray.push([pokemonLv3, pokemonLv3Img])
-      setPokemonEvolutions(pokemonEvoArray)
+      if (data.chain.evolves_to[0].evolves_to.length !== 0) {
+        let pokemonLv3 = data.chain.evolves_to[0].evolves_to[0].species.name;
+        let pokemonLv3Img = await getPokemonImgs(pokemonLv3);
+        pokemonEvoArray.push([pokemonLv3, pokemonLv3Img]);
       }
     }
-
-
-
+    setPokemonEvolutions(pokemonEvoArray);
   }
 
-  async function getPokemonImgs(pokemonName: string){
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}/`)
-    const data =  await response.json()
-    return data.sprites.other[`official-artwork`].
-      front_default
+  async function getPokemonImgs(pokemonName: string) {
+    const response = await fetch(
+      `https://pokeapi.co/api/v2/pokemon/${pokemonName}/`
+    );
+    const data = await response.json();
+    return data.sprites.other[`official-artwork`].front_default;
   }
-  
 
   //logica boton atras
   function prevClick() {
@@ -72,16 +72,18 @@ const App = () => {
   }
 
   return (
-    <>
-      <div className="card-container">
-        <Card />
+    <div className="app">
+      <div className={`card-container card${pokemonEvolutions.length} `}>
+        {pokemonEvolutions.map((pokemon) => (
+          <Card key={pokemon[0]} name={pokemon[0]} img={pokemon[1]} />
+        ))}
       </div>
 
       <div className="buttons-container">
         <Button icon={<TiArrowLeftOutline />} handleClick={prevClick} />
         <Button icon={<TiArrowRightOutline />} handleClick={nextClick} />
       </div>
-    </>
+    </div>
   );
 };
 
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,14 +1,18 @@
-import '../sass/Card.scss'
+import "../sass/Card.scss";
 
-const Card = () => {
-  return (
-    <div className="card">
-      <p className="card__name">Dana</p>
-      <div className="card__circle"></div>
-      <img className="card__img" src="" alt="pokemon img"/>
-
-    </div>
-  )
+interface CardProps {
+  name: string;
+  img: string;
 }
 
-export  {Card}
+const Card = ({ name, img }: CardProps) => {
+  return (
+    <div className="card">
+      <p className="card__name">{name}</p>
+      <div className="card__circle"></div>
+      <img className="card__img" src={img} alt="pokemon img" />
+    </div>
+  );
+};
+
+export { Card };
